46b6914e80bf7386fa7db73b50dab60ada192d1f
Channing-Moor <crmoor01@bvsd.org>
2024-10-29 21:54:44 +0000

    Finished practice 10

---
+++ goober.py
@@ -0,0 +1,12 @@
+"""
+Millifaersla problem:
+Channing Moor - October 2024
+"""
+
+
+def main() -> None:
+    s = input()
+    aloc: int = s.find(a)
+    print(s[:aloc])
+if __name__ == "__main__":
+  main()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """
-Type your header comment.
-Name - Month Year
+Kveoja problem:
+Channing Moor - October 2024
 """
 
+num_int = int(input("Enter the number of integers: "))
+total = 0
+for i in range(num_int):
+    new_int = int(input("Enter a value: "))
+    total += new_int
+print(total)
 
-def main() -> None:
-  pass
-
-
-if __name__ == "__main__":
-  main()

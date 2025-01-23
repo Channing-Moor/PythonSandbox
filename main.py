cdd292dbcafb25ddf3e959463955651c76579c40
Channing-Moor <crmoor01@bvsd.org>
2025-01-23 22:22:28 +0000

    Started are you a spy

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 """
-Kveoja problem:
-Channing Moor - October 2024
+Are-You-A-Spy problem:
+Channing Moor - January 2025
 """
 
 def main() -> None:
-    pass
+    # Get/setup greeting and response
+    grset: str = input()
+    grset = grset.lower()
+    greeting,response = grset.split("|")
+
+    # Get rid of non alphabetic characters
+    newresponse: str = 
+    for i in range(len(response)):
+        if response[i].isalpha() == False:
+            response.replace(response[i], "")
+
+    # Setup bool and check for anagram
+    for ch in response:
+        # If it is not a letter
+        elif ch not in greeting:
+            print("You're not a secret agent!")
+        else:
+            spy = False
+            break
+    
+    print("That's my secret contact!")
+
 
 if __name__ == "__main__":
-    
+    main()  
 
